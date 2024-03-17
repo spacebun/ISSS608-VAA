dbe6f517cc701cbabd83053a3a689c8415eb61c6
spacebun <serena.lew.2022@smu.edu.sg>
2024-03-18 01:56:07 +0800

    Complete THE4

--- Take-home_Ex/Take-home_Ex4/app.R
+++ Take-home_Ex/Take-home_Ex4/app.R
@@ -1,137 +1,94 @@
 ##### Set up #####
 pacman::p_load(gstat)
 pacman::p_load(tmap)
-pacman::p_load(shiny, shinydashboard, tidyverse, ggplot2, dplyr, lubridate, ggthemes, plotly, ggHoriPlot, dtwclust, factoextra)
+pacman::p_load(shiny, shinydashboard, shinyWidgets, tidyverse, ggplot2, dplyr, lubridate, ggthemes, plotly, sf, terra, viridis)
 
 weather_data <- read_rds("data/weather_imputed_11stations.rds") 
 
-weather_data <- weather_data %>% 
-  mutate(Date_mine = make_date(2023, month(Date), day(Date)),
-         Month_Name = factor(months(Date), levels = month.name)
-  )
-
 variables <- c("Daily Rainfall Total (mm)", "Mean Temperature (°C)", "Minimum Temperature (°C)", "Maximum Temperature (°C)")
 
-# mpsz2019 <- st_read(dsn = "data/geospatial", 
-#                     layer = "MPSZ-2019") %>%
-#   st_transform(crs = 3414)
+mpsz2019 <- st_read(dsn = "data/geospatial", layer = "MPSZ-2019") %>% st_transform(crs = 3414)
 
 ##### Header and sidebar #####
 header <- dashboardHeader(title = "Singapore Weather Analytics (2021-2023)")
 
 sidebar <- dashboardSidebar(
   width = 100,
-  tags$head(tags$style(HTML("
-      .sidebar-menu > li > a {
-        white-space: normal; 
-        line-height: 1.2;
-      }
-    "))
+  tags$head(tags$style(HTML(".sidebar-menu > li > a {white-space: normal; line-height: 1.2;}"))
   ),
   sidebarMenu(
     menuItem("Landing Page", tabName = "LandingPage"),
     menuItem("EDA & CDA", tabName = "EDACDA"),
     menuItem("Univariate Forecasting", tabName = "Univariate"),
-    menuItem("Cluster & Group Forecasting", tabName = "Cluster")
+    menuItem("Spatial Interpolation", tabName = "Geospatial")
     )
   )
 
-##### HoriPlot ##### 
-HoriPlot <-fluidPage(
+##### Geospatial UI  #####
+GeospatialUI <- fluidPage(
+  
   # Row 1
   fluidRow(
-    # Control 
-    box(
-      title = "Controls", width = 3, status = "primary", solidHeader = TRUE,
-      radioButtons("selectedVariable", "Choose variable", variables),
-      radioButtons("compareAcrossHorizon", "Compare across", c("Stations", "Years")),
-      uiOutput("dynamicUIHoriPlot"),  # Placeholder for dynamic UI components
-      actionButton("updateHoriPlot", "Update Plot")
+    box(title = "Data Selection Parameters",  width = 2, status = "primary", solidHeader = TRUE,
+        radioButtons("GS_selected_var", "Choose variable", variables),
+        radioButtons("GS_time_resolution", "Time resolution", c("Day", "Month", "Year")),
+        uiOutput("GS_dynamic_time_resolution"), 
+        actionButton("GS_updatetmap", "Show map")
+      
     ),
-    
-    # Output Plot
-    box(
-      title = "Horizon Plot", width = 9, status = "primary", solidHeader = TRUE,
-      collapsible = TRUE,
-      plotOutput("horizonPlot")
-    )
+    box(title = "Map",  width = 10, status = "primary", solidHeader = TRUE,
+        tmapOutput("GS_tmap")
+        )
   ),
-  
-  # Row 2
   fluidRow(
-    # Output table
-    box(
-      title = "Table", width = 12, status = "primary", solidHeader = TRUE,
-      collapsible = TRUE,
-      DT::dataTableOutput("horizonDataTable")
+    box(title = "IDW Selection Parameters",  width = 2, status = "primary", solidHeader = TRUE,
+        radioButtons("GS_show_IDW", "Show IDW?", c("Yes", "No"), selected = "Yes", inline = TRUE),
+        uiOutput("GS_dynamic_IDW"),
+        actionButton("GS_updateIDW", "Show Result")
+        
+    ),
+    box(title = "IDW Map",  width = 10, status = "primary", solidHeader = TRUE,
+        plotOutput("GS_IDW_map")
+    )
+    
+  ),
+  fluidRow(
+    box(title = "Kriging Selection Parameters",  width = 2, status = "primary", solidHeader = TRUE,
+        radioButtons("GS_show_OK", "Show Kriging?", c("Yes", "No"), selected = "Yes", inline = TRUE),
+        uiOutput("GS_dynamic_OK"),
+        actionButton("GS_updateOK", "Show Result")
+        
+    ),
+    box(title = "Ordinary Kriging Map",  width = 10, status = "primary", solidHeader = TRUE,
+        plotOutput("GS_OK_map")
     )
     
   )
 )
 
-##### Clustering ##### 
-Clustering <-fluidRow(
-  box(title = "Controls", width = 3, status = "primary", solidHeader = TRUE,
-      radioButtons("selectedVariable", "Choose variable", variables),
-      radioButtons("clusterBy", "Cluster by", c("Stations", "Months") ),
-      selectInput("selectedClusterMethod", "Choose cluster method", c("partitional", "hierarchical", "tadpole","fuzzy")),
-      sliderInput("numClusters", "Number of Clusters", 
-                  min = 2, max = 10, value = 6),
-      uiOutput("dynamicUIClustering"),  # Placeholder for dynamic UI components
-      actionButton("updateClustering", "Update Clustering")
-      ),
-  box(title = "Clusters", width = 9, status = "primary", solidHeader = TRUE,
-      collapsible = TRUE,
-    plotOutput("clusterDendrogram")
-  )
-)
 
-##### Forecasting ##### 
-Forecasting <-fluidRow(
-  box()
-)
-
-# Define sub tabs needed for "Cluster & Group Forecasting" tabName "Cluster"
-clusterSubTabs <- tabsetPanel(
-  tabPanel("Horizon Plot",
-           HoriPlot),
-  tabPanel("Clustering",
-           Clustering),
-  tabPanel("Forecasting",
-           Forecasting)
-)
-
-
-# Body content
+##### Body content #####
 body <- dashboardBody(
   tabItems(
     # First tab content
     tabItem(tabName = "LandingPage",
             fluidRow(
-              box(plotOutput("plot1", height = 250)),
-              
-              box(
-                title = "Controls",
-                sliderInput("slider", "Number of observations:", 1, 100, 50)
-              )
+              box()
             )
     ),
     
     # Second tab content
     tabItem(tabName = "EDACDA",
             h2("EDACDA content")
-            
     ),    
     # Second tab content
     tabItem(tabName = "Univariate",
             h2("Univariate content")
-            
     ),    
     # Second tab content
-    tabItem(tabName = "Cluster",
-            h2("Clustering and Group Forecasting"),
-            clusterSubTabs
-            
+    tabItem(tabName = "Geospatial",
+            h2("Spatial Interpolation"),
+            GeospatialUI
     )
   )
 )
@@ -145,181 +102,111 @@ ui <- dashboardPage(
 
 ##### server #####
 server <- function(input, output) {
+  ##### Spatial Interpolation ##### 
   
-  ##### Dummy #####
-  set.seed(122)
-  histdata <- rnorm(500)
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
-  })
-  
-  ##### For HoriPlot #####
-  # Dynamic UI
-  output$dynamicUIHoriPlot <- renderUI({
-    if (input$compareAcrossHorizon == "Years") {
-      list(
-        selectInput("selectedStation", "Choose station", choices = unique(weather_data$Station), selected = unique(weather_data$Station)[1])
-      )
-    } else if (input$compareAcrossHorizon == "Stations") {
-      list(
-        selectInput("selectedStations", "Choose stations", 
-                    choices = unique(weather_data$Station), 
-                    multiple = TRUE,
-                    selected = unique(weather_data$Station)[1]),
-        selectInput("selectedYear", "Choose Year", choices = unique(weather_data$Year), selected = unique(weather_data$Year)[1])
-      )
+  # tmap
+  # Dynamic UI for selected time_resolution
+  output$GS_dynamic_time_resolution <- renderUI({
+    if (input$GS_time_resolution == "Day") {
+        airDatepickerInput("GS_selected_date", label = "Select date", value = "2021-01-01", maxDate = "2023-12-31", minDate = "2021-01-01", dateFormat = "yyyy-MM-dd")
+    } else if (input$GS_time_resolution == "Month") {
+        airMonthpickerInput("GS_selected_month", label = "Select month and year", value = "2021-01-01", maxDate = "2023-12-31", minDate = "2021-01-01", dateFormat = "yyyy-MM")
+    } else if (input$GS_time_resolution == "Year") {
+      airYearpickerInput("GS_selected_year", label = "Select year", value = "2021-01-01", maxDate = "2023-12-31", minDate = "2021-01-01", dateFormat = "yyyy")
     }
   })
   
-  # Reactive update button
-  reactiveDataHoriPlot <- eventReactive(input$updateHoriPlot, {
+  output$GS_tmap <- renderTmap({
+    req(input$GS_updatetmap) # Ensure action button has been pressed. This needs to be re-worked. 
+    selected_var <- input$GS_selected_var 
     
-    selected_var <- input$selectedVariable
-    
-    
-    if (input$compareAcrossHorizon == "Years") {
-      filtered_data <- weather_data %>%
-        filter(Station %in% input$selectedStation)
-    } else if (input$compareAcrossHorizon == "Stations") {
-      filtered_data <- weather_data %>%
-        filter(Station %in% input$selectedStations, Year == input$selectedYear)
+    # Prepare data based on user input
+    if (input$GS_time_resolution == "Day") {
+      selected_date <- as.Date(input$GS_selected_date)
+      variable_data <- weather_data %>%
+        filter(Date == selected_date)
+      # Group and summarise data, selecting for the correctly variable
+      variable_data <- variable_data %>%
+        group_by(Station, Date, LAT, LONG) %>%
+        summarise(ValueToPlot = if(grepl("Rainfall", selected_var)) {
+          sum(.data[[selected_var]], na.rm = TRUE) # Sum or average based on the variable
+        } else if (grepl("Temperature", selected_var)) {
+          mean(.data[[selected_var]], na.rm = TRUE)
+        }, .groups = 'drop')
+    } else if (input$GS_time_resolution == "Month") {
+      selected_month <- format(as.Date(input$GS_selected_month), "%m")
+      selected_year <- format(as.Date(input$GS_selected_month), "%Y")
+      variable_data <- weather_data %>%
+        filter(format(Date, "%Y-%m") == paste0(selected_year, "-", selected_month))
+      # Group and summarise data, selecting for the correctly variable
+      variable_data <- variable_data %>%
+        group_by(Station, Year, Month, LAT, LONG) %>%
+        summarise(ValueToPlot = if(grepl("Rainfall", selected_var)) {
+          sum(.data[[selected_var]], na.rm = TRUE) # Sum or average based on the variable
+        } else if (grepl("Temperature", selected_var)) {
+          mean(.data[[selected_var]], na.rm = TRUE)
+        }, .groups = 'drop')
+    } else if (input$GS_time_resolution == "Year") {
+      selected_year <- as.character(format(input$GS_selected_year, "%Y"))
+      print(selected_year)
+      variable_data <- weather_data %>%
+        filter(format(Date, "%Y") == selected_year)
+      # Group and summarise data, selecting for the correctly variable
+      variable_data <- variable_data %>%
+        group_by(Station, Year, LAT, LONG) %>%
+        summarise(ValueToPlot = if(grepl("Rainfall", selected_var)) {
+          sum(.data[[selected_var]], na.rm = TRUE) # Sum or average based on the variable
+        } else if (grepl("Temperature", selected_var)) {
+          mean(.data[[selected_var]], na.rm = TRUE)
+        }, .groups = 'drop')
     }
-    
-    facet_var = ifelse(input$compareAcrossHorizon == "Years", "Year", "Station")
-    
-    title <- ifelse(input$compareAcrossHorizon == "Years",
-                            paste(selected_var, "for", input$selectedStation, "across the years 2021 to 2023"),
-                            paste(selected_var, "for", input$selectedYear, "across station(s)"))
-    
-    return(list(data = filtered_data, var = selected_var, facet = facet_var, title = title))
-  })
-  
-  # Output plot
-  output$horizonPlot <- renderPlot({
-    # Obtain the reactive data
-    res <- reactiveDataHoriPlot()
-    data_to_plot <- res$data
-    selected_var <- res$var
-    facet_var <- res$facet
-    title <- res$title
-    
-    
-    # Compute origin and  horizon scale cutpoints: 
-    cutpoints <- data_to_plot %>% 
-      mutate(
-        outlier = between(
-          .data[[selected_var]], 
-          quantile(.data[[selected_var]], 0.25, na.rm = TRUE) -
-            1.5 * IQR(.data[[selected_var]], na.rm = TRUE),
-          quantile(.data[[selected_var]], 0.75, na.rm = TRUE) +
-            1.5 * IQR(.data[[selected_var]], na.rm = TRUE))) %>% 
-      filter(outlier)
-    
-    ori <- sum(range(cutpoints[[selected_var]]))/2
-    sca <- seq(range(cutpoints[[selected_var]])[1], range(cutpoints[[selected_var]])[2], length.out = 7)[-4]
-    
-    ori <- round(ori, 2) # The origin, rounded to 2 decimal places
-    sca <- round(sca, 2) # The horizon scale cutpoints
 
-    # Plot horizon plot
-    data_to_plot %>% ggplot() +
-      geom_horizon(aes(x = Date_mine, 
-                       y = .data[[selected_var]],
-                       fill = after_stat(Cutpoints)), 
-                   origin = ori, horizonscale = sca) +
-      scale_fill_hcl(palette = 'RdBu', reverse = T) +
-      facet_grid(as.formula(paste(facet_var, "~ ."))) +
-      theme_few() +
-      theme(
-        panel.spacing.y = unit(0, "lines"),
-        strip.text.y = element_text(size = 7, angle = 0, hjust = 0),
-        axis.text.y = element_blank(),
-        axis.title.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.border = element_blank()
-      ) +
-      scale_x_date(expand = c(0, 0), 
-                   date_breaks = "1 month", 
-                   date_labels = "%b") +
-      xlab('Date') +
-      ggtitle(title)
-    })
-  output$horizonDataTable <- DT::renderDataTable({
-    res <- reactiveDataHoriPlot()
-    data_to_plot <- res$data
-    selected_var <- res$var
-    data_to_table <- data_to_plot %>%
-      select(c(Station, Date, Year, Month, Day, selected_var))
+    # Replace 'mpsz2019' with your spatial boundary data if different
+    
+    # Check if there's data to plot
+    if ("ValueToPlot" %in% names(variable_data) && nrow(variable_data) > 0) {
+      variable_data_sf <- st_as_sf(variable_data, coords = c("LONG", "LAT"), crs = 4326) %>% st_transform(crs = 3414)
       
-    DT::datatable(data_to_table,
-                  class= "compact",
-                  rownames = FALSE,
-                  width="100%", 
-                  options = list(pageLength = 10, scrollX=T))  # Assuming 'weather_data' is your dataframe
-  })
-  
-  ##### For Clustering #####
-  # Dynamic UI
-  output$dynamicUIClustering <- renderUI({
-    if (input$clusterBy == "Months") {
-      list(
-        selectInput("selectedStation", "Choose station", choices = unique(weather_data$Station), selected = unique(weather_data$Station)[1]),
-        selectInput("selectedYear", "Choose Year", choices = unique(weather_data$Year), selected = unique(weather_data$Year)[1])
-      )
-    } else if (input$clusterBy == "Stations") {
-      list(
-        selectInput("selectedStations", "Choose stations", 
-                    choices = unique(weather_data$Station), 
-                    multiple = TRUE,
-                    selected = unique(weather_data$Station)[1])
-      )
+      tmap_options(check.and.fix = TRUE) # Use this code to fix topo error without changing the data. 
+      tmap_mode("view") # Make map interactive
+      tm <- tm_shape(mpsz2019) + tm_borders() + tm_shape(variable_data_sf) + tm_dots(col = 'ValueToPlot') # Use color to differentiate
+      tm
+    } else {
+      plot(1, type = "n", xlab = "", ylab = "", axes = FALSE)
+      text(1, 0.5, "No data available for the selected criteria", cex = 1.2)
     }
   })
   
-  reactiveDataClustering <- eventReactive(input$updateClustering, {
-    selected_var <- input$selectedVariable
-    
-    selected_data <- weather_data %>%
-      select(Station, Date, Year, Month, Day, .data[[selected_var]])
-    
-    if (input$clusterBy == "Months") {
-      selected_data <- selected_data %>% 
-        filter(Station %in% input$selectedStation, Year == input$selectedYear) %>%
-        group_by(Month) %>%
-        summarise(data_list = list(.data[[selected_var]]), .groups = 'drop')
-      
-      list_of_series <- selected_data$data_list
-    } else if (input$clusterBy == "Stations") {
-      # Placeholder for when 'Stations' is the selected clusterBy option
-      # Implement according to your specific requirements
+  # IDW
+  output$GS_dynamic_IDW <- renderUI({
+    if (input$GS_show_IDW == "Yes") {
+      list(
+        sliderInput("GS_IDW_res", "Resolution", 
+                    min = 30, max = 80, value = 50),
+        sliderInput("GS_nmax" , "nmax",
+                    min = 1, max = 10, value = 3)
+        
+      )
     }
     
-    # Ensure to return relevant data that your subsequent plotting/logic expects
-    return(list(data = selected_data, var = selected_var))
   })
   
-  
-  # Output clusterDendrogram
-  output$clusterDendrogram <- renderPlot({
-    res <- reactiveDataClustering()
-    list_of_series <- res$data$data_list
-    names(list_of_series)<-month.name
-    print(str(list_of_series))
-    n_cluster <- input$numClusters # Need to shift this to reactive
-    c <- tsclust(series = list_of_series, 
-                 type = "hierarchical",
-                 k = n_cluster, 
-                 distance = "dtw", 
-                 control = hierarchical_control(method = "complete"))
-    p <- fviz_dend(c, k = n_cluster,
-                   cex = 0.5, 
-                   k_colors = c("jco"),
-                   color_labels_by_k = FALSE,
-                   rect_border = "jco",
-                   rect = TRUE,
-                   rect_fill = TRUE)
-    print(p)
+  # OK
+  output$GS_dynamic_OK <- renderUI({
+    if (input$GS_show_OK == "Yes") {
+      model_choices <- c("Nug", "Exp","Sph","Gau","Exc","Mat","Ste","Cir","Lin","Bes","Pen","Per","Wav","Hol","Log","Pow","Spl")
+      # min_nugget <- min(variable_data$ValueToPlot, na.rm = TRUE)
+      # max_nugget <- max(variable_data$ValueToPlot, na.rm = TRUE)
+      # mid_nugget  <- mean(variable_data$ValueToPlot, na.rm = TRUE)
+      list(
+        sliderInput("GS_OK_res", "Resolution", min = 30, max = 80, value = 50),
+        selectInput("GS_model" , "model", model_choices),
+        sliderInput("GS_OK_psill", "psill", min = 0.5, max = 10, value = 0.5, step = 0.5),
+        sliderInput("GS_OK_range", "range", min = 500, max = 10000, value = 8000, step = 500),
+        sliderInput("GS_OK_nugget", "nugget", min = 0.1, max = 10, value = 0.1, step = 0.1)
+      )
+    }
+    
   })
   
 }
